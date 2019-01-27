a572a5cb60f46f2759c92d4ea62174c7d7fb4a7d
千田聖也 <wa718077@gmail.com>
2019-01-27 11:35:35 +0900

    Integrated form action

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,53 @@
+class ArticlesController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @articles = Article.order(created_at: :desc)
+  end
+
+  def show 
+  end
+
+  def new
+    @articles = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @articles = Article.new(article_params)
+    if @articles.save
+      redirect_to @articles, notice:'Created'
+    else
+      render :new, alert: 'Not created'
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @articles, notice:'Updated'
+    else
+      render :edit, alert: 'Not updated'
+    end
+  end
+
+  def destroy
+    if @articles.destroy
+      redirect_to root_path, notice:'Succeeded'
+    else
+      redirect_to root_path, alert: 'Not deleted'
+    end
+  end
+
+  private
+  
+  def find_post
+    @articles = Article.find(params[:id])
+  end
+
+  def article_params 
+    params.require(:article).permit(:title, :body)
+  end
+
+end
